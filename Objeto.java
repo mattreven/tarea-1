1ad8e248e3f28f273dec3f28598e636d322736d1
mattreven <mattreven@hotmail.com>
2022-09-07 13:40:39 -0500

    Objeto.java

---
+++ Objeto.java
@@ -0,0 +1,14 @@
+
+public class Objeto{
+  private int numero;
+
+  public Objeto(int numero){
+    this.numero = numero;
+  }
+  public int getNumero(){
+    return numero;
+  }
+  public void setNumero(int n){
+    this.numero = n;
+  }
+}
